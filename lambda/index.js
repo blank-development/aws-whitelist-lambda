268232c3a9353cb84ecd2a4f9d249ce5a51722ba
mirkopezo <fireover@tutanota.com>
2022-05-16 11:32:44 +0200

    Add comments to lambda script

--- lambda/index.js
+++ lambda/index.js
@@ -11,20 +11,30 @@ exports.handler = async (event) => {
   let proof;
 
   switch (phase) {
+    // First whitelist phase
     case "0":
       const tree_1 = new MerkleTree(leaves_1, keccak256, { sortPairs: true });
+      // Return a proof from first whitelist Merkle tree
       proof = tree_1.getHexProof(leaf);
       break;
+    // Second whitelist phase
     case "1":
       const tree_2 = new MerkleTree(leaves_2, keccak256, { sortPairs: true });
+      // Get a proof from second whitelist Merkle tree
       const proof_temp = tree_2.getHexProof(leaf);
+      // If that proof exists
       if (proof_temp.length !== 0) {
+        // Return a proof from second whitelist Merkle tree
         proof = proof_temp;
-      } else {
+      }
+      // Otherwise check first whitelist Merkle tree
+      else {
         const tree_1 = new MerkleTree(leaves_1, keccak256, { sortPairs: true });
+        // Return a proof from first whitelist Merkle tree
         proof = tree_1.getHexProof(leaf);
       }
       break;
+    // Public mint phase
     default:
       proof = [];
   }
